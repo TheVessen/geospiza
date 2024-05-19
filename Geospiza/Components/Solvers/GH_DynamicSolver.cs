ea25467cb103a2c6d8c7590cf8be6a48c32155d9
VincentRun <48569186+TheVessen@users.noreply.github.com>
2024-05-19 11:00:40 +0200

    Add k-Means and add documentation

---
+++ Geospiza/Components/Solvers/GH_DynamicSolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Geospiza;
+
+public class GH_DynamicSolver : GH_Component
+{
+
+    /// <summary>
+    /// Initializes a new instance of the GH_DynamicSolver class.
+    /// </summary>
+    public GH_DynamicSolver()
+        : base("GH_DynamicSolver", "GH_DynamicSolver",
+            "Description",
+            "Geospiza", "Subcategory")
+    {
+    }
+
+    /// <summary>
+    /// Registers all the input parameters for this component.
+    /// </summary>
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
+    {
+    }
+
+    /// <summary>
+    /// Registers all the output parameters for this component.
+    /// </summary>
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+    {
+    }
+
+    /// <summary>
+    /// This is the method that actually does the work.
+    /// </summary>
+    /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+    protected override void SolveInstance(IGH_DataAccess DA)
+    {
+    }
+
+    /// <summary>
+    /// Provides an Icon for the component.
+    /// </summary>
+    protected override Bitmap Icon
+    {
+        get
+        {
+            //You can add image files to your project resources and access them like this:
+            // return Resources.IconForThisComponent;
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Gets the unique ID for this component. Do not change this ID after release.
+    /// </summary>
+    public override Guid ComponentGuid
+    {
+        get { return new Guid("866F5DF3-2C87-4882-81B7-68F9380E316A"); }
+    }
+}
--- Geospiza/src/Core/StateManager.cs
+++ Geospiza/src/Core/StateManager.cs
@@ -4,130 +4,156 @@ using Geospiza.Comonents;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Special;
 
-namespace Geospiza.Core;
-
-public class StateManager
+namespace Geospiza.Core
 {
-  private static readonly Dictionary<GH_BasicSolver, StateManager> Instances = new();
-  public Dictionary<Guid, dynamic> AllGenePools;
-  public Dictionary<Guid, GH_NumberSlider> AllSliders;
-  private GH_Document _document;
-
-  private StateManager()
-  {
-  }
-
-  public GH_Component ThisComponent { get; private set; }
-  public int NumberOfGeneIds { get; set; }
-  public int PreviewLevel { get; set; } = 0;
-  public Fitness FitnessComponent { get; private set; }
-  public Dictionary<Guid, TemplateGene> Genotype { get; private set; }
-
-  public static StateManager GetInstance(GH_BasicSolver solver)
-  {
-    if (!Instances.ContainsKey(solver)) Instances[solver] = new StateManager();
-    return Instances[solver];
-  }
-
-  public void SetDocument(GH_Document document)
-  {
-    if (_document != null) return;
-
-    _document = document;
-  }
-
-  public GH_Document GetDocument()
-  {
-    return _document;
-  }
-
-  public void SetFitnessComponent()
-  {
-    if (FitnessComponent != null) return;
-
-    foreach (var comp in _document.Objects)
-      if (comp is Fitness fitness)
-        FitnessComponent = fitness;
-    if (FitnessComponent == null)
-      ThisComponent.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No fitness component found");
-  }
-
-  public void SetGenes(List<string> geneIds)
-  {
-    if (Genotype == null)
+    /// <summary>
+    /// Manages the state of the application, including the gene pools and sliders.
+    /// </summary>
+    public class StateManager
     {
-      InitializeGenePool(geneIds, _document);
-      NumberOfGeneIds = geneIds.Count;
-    }
-    else if (NumberOfGeneIds != geneIds.Count)
-    {
-      Reset();
-      NumberOfGeneIds = geneIds.Count;
-      InitializeGenePool(geneIds, _document);
-    }
-  }
+        // Stores instances of StateManager for each GH_BasicSolver
+        private static readonly Dictionary<GH_BasicSolver, StateManager> Instances = new();
+        public Dictionary<Guid, dynamic> AllGenePools;
+        public Dictionary<Guid, GH_NumberSlider> AllSliders;
+        private GH_Document _document;
 
-  private void Reset()
-  {
-    FitnessComponent = null;
-    Genotype = null;
-    ThisComponent = null;
-    AllSliders = null;
-    AllGenePools = null;
-  }
-
-  public void SetThisComponent(GH_Component component)
-  {
-    if (ThisComponent != null) return;
-    ThisComponent = component;
-  }
-
-  /// <summary>
-  ///   Initializes a gene pool from a list of gene IDs and a GH_Document.
-  /// </summary>
-  /// <param name="geneIds">A list of gene IDs to initialize the gene pool with.</param>
-  /// <param name="doc">The GH_Document to use for finding objects.</param>
-  /// <returns>A dictionary of genes where the key is a Guid and the value is a Gene object.</returns>
-  private void InitializeGenePool(List<string> geneIds, GH_Document doc)
-  {
-    var genes = new Dictionary<Guid, TemplateGene>();
-    var genePools = new Dictionary<Guid, dynamic>();
-    var numberSliders = new Dictionary<Guid, GH_NumberSlider>();
-
-    foreach (var id in geneIds)
-    {
-      var currentParam = doc.FindObject(new Guid(id), true);
-      var currentType = currentParam.GetType().ToString();
-      var guid = currentParam.InstanceGuid;
-
-      switch (currentType)
-      {
-        case "GalapagosComponents.GalapagosGeneListObject":
+        private StateManager()
         {
-          var genePool = (dynamic)currentParam;
-          genePools[guid] = genePool;
-          if (genePool.Count != 0)
-            for (var i = 0; i < genePool.Count; i++)
+        }
+
+        public GH_Component ThisComponent { get; private set; }
+        public int NumberOfGeneIds { get; set; }
+        public int PreviewLevel { get; set; } = 0;
+        public Fitness FitnessComponent { get; private set; }
+        public Dictionary<Guid, TemplateGene> Genotype { get; private set; }
+
+        /// <summary>
+        /// Returns the instance of StateManager for the given solver.
+        /// </summary>
+        public static StateManager GetInstance(GH_BasicSolver solver)
+        {
+            if (!Instances.ContainsKey(solver)) Instances[solver] = new StateManager();
+            return Instances[solver];
+        }
+
+        /// <summary>
+        /// Sets the document for the StateManager.
+        /// </summary>
+        public void SetDocument(GH_Document document)
+        {
+            if (_document != null) return;
+
+            _document = document;
+        }
+
+        /// <summary>
+        /// Returns the document of the StateManager.
+        /// </summary>
+        public GH_Document GetDocument()
+        {
+            return _document;
+        }
+
+        /// <summary>
+        /// Sets the fitness component for the StateManager.
+        /// </summary>
+        public void SetFitnessComponent()
+        {
+            if (FitnessComponent != null) return;
+
+            foreach (var comp in _document.Objects)
+                if (comp is Fitness fitness)
+                    FitnessComponent = fitness;
+            if (FitnessComponent == null)
+                ThisComponent.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No fitness component found");
+        }
+
+        /// <summary>
+        /// Sets the genes for the StateManager.
+        /// </summary>
+        public void SetGenes(List<string> geneIds)
+        {
+            if (Genotype == null)
             {
-              var gene = new TemplateGene(genePool, i);
-              genes[gene.GeneGuid] = gene;
+                InitializeGenePool(geneIds, _document);
+                NumberOfGeneIds = geneIds.Count;
+            }
+            else if (NumberOfGeneIds != geneIds.Count)
+            {
+                Reset();
+                NumberOfGeneIds = geneIds.Count;
+                InitializeGenePool(geneIds, _document);
+            }
+        }
+
+        /// <summary>
+        /// Resets the StateManager.
+        /// </summary>
+        private void Reset()
+        {
+            FitnessComponent = null;
+            Genotype = null;
+            ThisComponent = null;
+            AllSliders = null;
+            AllGenePools = null;
+        }
+
+        /// <summary>
+        /// Sets the component for the StateManager.
+        /// </summary>
+        public void SetThisComponent(GH_Component component)
+        {
+            if (ThisComponent != null) return;
+            ThisComponent = component;
+        }
+
+        /// <summary>
+        /// Initializes a gene pool from a list of gene IDs and a GH_Document.
+        /// </summary>
+        /// <param name="geneIds">A list of gene IDs to initialize the gene pool with.</param>
+        /// <param name="doc">The GH_Document to use for finding objects.</param>
+        /// <returns>A dictionary of genes where the key is a Guid and the value is a Gene object.</returns>
+        private void InitializeGenePool(List<string> geneIds, GH_Document doc)
+        {
+            var genes = new Dictionary<Guid, TemplateGene>();
+            var genePools = new Dictionary<Guid, dynamic>();
+            var numberSliders = new Dictionary<Guid, GH_NumberSlider>();
+
+            foreach (var id in geneIds)
+            {
+                var currentParam = doc.FindObject(new Guid(id), true);
+                var currentType = currentParam.GetType().ToString();
+                var guid = currentParam.InstanceGuid;
+
+                switch (currentType)
+                {
+                    case "GalapagosComponents.GalapagosGeneListObject":
+                    {
+                        var genePool = (dynamic)currentParam;
+                        genePools[guid] = genePool;
+                        if (genePool.Count != 0)
+                            for (var i = 0; i < genePool.Count; i++)
+                            {
+                                var gene = new TemplateGene(genePool, i);
+                                genes[gene.GeneGuid] = gene;
+                            }
+
+                        break;
+                    }
+                    case "Grasshopper.Kernel.Special.GH_NumberSlider":
+                    {
+                        var sliderGene = currentParam as GH_NumberSlider;
+                        numberSliders[guid] = sliderGene;
+                        var gene = new TemplateGene(sliderGene);
+                        genes[gene.GeneGuid] = gene;
+                        break;
+                    }
+                }
             }
 
-          break;
+            Genotype = genes;
+            AllSliders = numberSliders;
+            AllGenePools = genePools;
         }
-        case "Grasshopper.Kernel.Special.GH_NumberSlider":
-        {
-          var sliderGene = currentParam as GH_NumberSlider;
-          numberSliders[guid] = sliderGene;
-          var gene = new TemplateGene(sliderGene);
-          genes[gene.GeneGuid] = gene;
-          break;
-        }
-      }
     }
-
-    Genotype = genes;
-    AllSliders = numberSliders;
-    AllGenePools = genePools;
-  }
 }
